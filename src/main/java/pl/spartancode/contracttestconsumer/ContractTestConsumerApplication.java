f1e84605ded96eab93ee8c969bc5d77b0768c8a3
Adam Lewandowski <adamlew@agora.pl>
2024-02-29 08:39:35 +0100

    initial commit

---
+++ src/main/java/pl/spartancode/contracttestconsumer/ContractTestConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.spartancode.contracttestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractTestConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractTestConsumerApplication.class, args);
+    }
+
+}
