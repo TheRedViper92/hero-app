ca2aa447749f9a6e79c3d2b165bbd8314433df2f
Adam <adam.lewandowski92@gmail.com>
2024-03-13 14:24:39 +0100

    updated main layout

--- src/main/java/pl/spartancode/heroapp/ui/MainLayout.java
+++ src/main/java/pl/spartancode/heroapp/ui/MainLayout.java
@@ -2,12 +2,19 @@ package pl.spartancode.heroapp.ui;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding.Horizontal;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding.Vertical;
 
+@CssImport("./styles/styles.css")
 public class MainLayout extends AppLayout {
     private final DrawerConfig drawerConfig;
 
@@ -18,21 +25,25 @@ public class MainLayout extends AppLayout {
     }
 
     private void createHeader() {
-        H1 logo = new H1("Hero Service");
-        logo.addClassNames("text-l", "m-m");
+        H1 appTitle = new H1("Hero Service");
+        appTitle.addClassNames(
+            FontSize.LARGE,
+            Margin.MEDIUM);
 
         DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassName("drawer-toggle");
+        toggle.setIcon(new Icon("lumo", "menu"));
 
-        HorizontalLayout header = new HorizontalLayout(toggle, logo);
-        header.setVerticalComponentAlignment(Alignment.CENTER);
-        header.setWidth("100%");
-        header.addClassName("header");
-
+        HorizontalLayout header = new HorizontalLayout(toggle, appTitle);
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.setWidthFull();
+        header.addClassNames(Vertical.NONE, Horizontal.MEDIUM);
         addToNavbar(header);
     }
 
     private void createDrawer() {
         RouterLink[] routerLinks = drawerConfig.getRouterLinks();
         addToDrawer(new VerticalLayout(routerLinks));
+        setDrawerOpened(false);
     }
 }
